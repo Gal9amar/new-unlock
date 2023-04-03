da216c747f5071e400e68fc99939881bf8a63682
Gal Amar <106630289+Gal9amar@users.noreply.github.com>
2023-04-04 01:04:17 +0300

    new update

--- js/gal.js
+++ js/gal.js
@@ -1,7 +1,7 @@
 function buyWithWhatsApp(item, price) {
   const message = `היי, אני מעוניין לקנות את המוצר ${item} במחיר של ${price}`;
   const encodedMessage = encodeURIComponent(message);
-  const whatsappLink = `https://wa.me/+972522010200?text=${encodedMessage}`;
+  const whatsappLink = `https://wa.me/+972533888381?text=${encodedMessage}`;
   window.open(whatsappLink);
 }
 
@@ -9,3 +9,11 @@ function buyWithWhatsApp(item, price) {
 
 const buyNowButton = document.querySelector('.buy-now-button');
 buyNowButton.addEventListener('click', buyWithWhatsApp);
+
+$(document).ready(function(){
+  $('#my_slider').carousel({
+    interval: 3000, // set the interval between slides in milliseconds
+    pause: 'hover', // pause the carousel on mouse hover
+    wrap: true // allow the carousel to loop back to the beginning
+  });
+});
