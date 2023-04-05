0a97ff3e9992d3511a3eab6b75e6692ebb550986
Gal Amar <106630289+Gal9amar@users.noreply.github.com>
2023-04-05 16:41:59 +0300

    new ver 2.0

--- js/gal.js
+++ js/gal.js
@@ -1,3 +1,24 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("back-to-top-btn").style.display = "block";
+  } else {
+    document.getElementById("back-to-top-btn").style.display = "none";
+  }
+}
+function scrollToTop() {
+  var position =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  if (position) {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+}
+
+
 function buyWithWhatsApp(item, price) {
   const message = `היי, אני מעוניין לקנות את המוצר ${item} במחיר של ${price}`;
   const encodedMessage = encodeURIComponent(message);
@@ -17,3 +38,5 @@ $(document).ready(function(){
     wrap: true // allow the carousel to loop back to the beginning
   });
 });
+
+
