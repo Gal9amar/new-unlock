59a555a4cebe1e6a817378a01f87d29d277bf39f
Gal Amar <106630289+Gal9amar@users.noreply.github.com>
2023-11-05 00:11:08 +0200

    נ

--- js/custom.js
+++ js/custom.js
@@ -39,3 +39,21 @@ $(".client_owl-carousel").owlCarousel({
 //     };
 //     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 // }
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => alert("תודה ששלחת לנו הודעה . נחזור בהקדם"))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
